1eaa0d21078f226075d7611d6d3ffb5dd6fe94b2
James Fitzpatrick <james.fitzy86y@gmail.com>
2025-03-30 19:14:27 +0100

    added 404 & 500 error handlers and force 500 route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 from models import get_greeting
 
 app = Flask(__name__)
@@ -24,6 +24,17 @@ def projects():
 def skills():
     return render_template('skills.html')
 
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('errors/404.html', error=error), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('errors/500.html', error=error), 500
+
+@app.route('/raise_500')
+def raise_500_error():
+    abort(500)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
