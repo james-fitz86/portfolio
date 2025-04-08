7470358473b9ede623e89fcfc95eaf8580ea268e
James Fitzpatrick <james.fitzy86y@gmail.com>
2025-04-08 13:52:23 +0100

    register error handlers to app

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from routes import routes_blueprint
+from routes import routes_blueprint, register_error_handlers
 from models import init_shelve
 
 app = Flask(__name__)
@@ -9,6 +9,8 @@ app.secret_key = "Replace me with a real secret key for production use"
 init_shelve()
 
 app.register_blueprint(routes_blueprint)
+
+register_error_handlers(app)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
