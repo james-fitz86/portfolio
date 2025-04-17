76cbb594e288d0f2584f4c0a743f5ce7ef5600af
James Fitzpatrick <james.fitzy86y@gmail.com>
2025-04-17 10:58:07 +0100

    added secret_key from .env

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from routes import routes_blueprint, register_error_handlers
 from models import init_shelve
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
 
-app.secret_key = "Replace me with a real secret key for production use"
+app.secret_key = os.getenv("SECRET_KEY")
 
 init_shelve()
 
